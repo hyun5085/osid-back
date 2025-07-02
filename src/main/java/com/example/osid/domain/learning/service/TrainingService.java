e0689cfba6a24dbe4e70bff293216538708cbfa8
ksw9250 <ksw9250@naver.com>
2025-07-02 16:24:30 +0900

    Test

--- src/main/java/com/example/osid/domain/learning/service/TrainingService.java
+++ src/main/java/com/example/osid/domain/learning/service/TrainingService.java
@@ -24,7 +24,7 @@ public class TrainingService {
         try {
             System.out.println("[스케줄러] train.py 실행 시작");
 
-            ProcessBuilder pb = new ProcessBuilder("app/osid-back/venv/bin/python", "train.py");
+            ProcessBuilder pb = new ProcessBuilder("/app/venv/bin/python", "train.py");
             pb.redirectErrorStream(true);
             Process process = pb.start();
 
